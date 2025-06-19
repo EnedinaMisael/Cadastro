691b38cfc5bd204b781f6d59586e5777437977b3
EnedinaMisael <enedina_braga.porto@hotmail.com>
2025-06-18 21:18:10 -0300

    primeiro commit

---
+++ src/main/java/com/eneds/spring_app_cadastro/SpringAppCadastroApplication.java
@@ -0,0 +1,13 @@
+package com.eneds.spring_app_cadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppCadastroApplication.class, args);
+	}
+
+}
