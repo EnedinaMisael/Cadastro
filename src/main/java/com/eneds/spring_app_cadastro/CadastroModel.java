3183da9559c9d38c436ce6dc600289fd45a42270
EnedinaMisael <enedina_braga.porto@hotmail.com>
2025-06-19 09:22:03 -0300

    criando entidade no banco atraves do model

---
+++ src/main/java/com/eneds/spring_app_cadastro/CadastroModel.java
@@ -0,0 +1,57 @@
+package com.eneds.spring_app_cadastro;
+
+import jakarta.persistence.*;
+
+
+// entity transforma uma classe em uma entidade do db
+@Entity
+@Table(name = "tb_cadastro")
+public class CadastroModel {
+    // como faco para mostrar para o java preencher o id
+    // com anotacao ID
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)// para gerar id sequencial
+    Long id;
+    String nome;
+    String email;
+    int idade;
+
+
+    // abaixo os contrutores
+    public CadastroModel(){
+
+    }
+
+    public CadastroModel(String nome, String email, int idade){
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+
+    //getters end setters
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
